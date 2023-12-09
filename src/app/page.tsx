a8716fb24103e31417f86ba170377dbb56f209ed
robinetta <alxr19@gmail.com>
2023-12-08 23:58:30 -0500

    test geocoding form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,51 @@
-import Link from "next/link";
+"use client"
+import { useEffect, useState } from "react";
+import LocationForm from "../components/locationComponent";
+
+
+interface UserLocation {
+
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+}
+
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
 
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+  const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    function getUserLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude, accuracy } = position.coords;
+        setUserLocation({ latitude, longitude, accuracy });
+      });
+    }
+
+    getUserLocation();
+  }, []);
+
+  const handleGetCoordinates = (lat: number, lng: number) => {
+    setCoordinates({ lat, lng });
+  };
+
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1 className="text-white">
+          Latitude: {userLocation?.latitude}, Longitude: {userLocation?.longitude}
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
-}
+        <h2>Accuracy: {userLocation?.accuracy}</h2>
+        <LocationForm onGetCoordinates={handleGetCoordinates} />
+        {coordinates && (
+        <p>Coordinates from form: Latitude: {coordinates.lat}, Longitude: {coordinates.lng}</p>
+      )}
+      </main>
+    );
+  }
+
